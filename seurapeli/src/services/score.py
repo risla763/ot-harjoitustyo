4a97e4c9957872ca1b01ae72f5a84329c10813aa
Maija Rislakki <maija.rislakki@helsinki.fi>
2023-06-28 21:18:47 +0300

    high score ehkä toimii

--- seurapeli/src/services/score.py
+++ seurapeli/src/services/score.py
@@ -21,6 +21,7 @@ class Score:
         #self.screen = pygame.display.set_mode((self.screen_width, self.screen_height))
         self.screen = screen
         self.counter = 0
+        self.switch = False
     def which_is_it(self,fin, fin2):
         """This method compares the sum of the players' cards in the round and calculates
         who wins that round
@@ -28,27 +29,30 @@ class Score:
             fin: the sum of the cards drawn by the player
             fin2: the sum of the cards drawn by the dealer
         Draws who won on the screen."""
-        if abs(fin-21) < abs(fin2-21):
-            if fin > 21 > fin2:
-                self.dealer += 1
-                draw_score(self,self.player,self.dealer)
-            else:
-                self.player += 1
-                draw_score(self,self.player,self.dealer)
-        elif abs(fin2-21) < abs(fin-21):
-            if fin2 > 21 > fin:
-                self.player += 1
-                draw_score(self,self.player,self.dealer)
-            else:
-                self.dealer += 1
-                draw_score(self,self.player,self.dealer)
-        self.saved_value = self.player
-        return self.player,self.dealer
+        if self.switch != True:
+            if abs(fin-21) < abs(fin2-21):
+                if fin > 21 > fin2:
+                    self.dealer += 1
+                    draw_score(self,self.player,self.dealer)
+                else:
+                    self.player += 1
+                    draw_score(self,self.player,self.dealer)
+            elif abs(fin2-21) < abs(fin-21):
+                if fin2 > 21 > fin:
+                    self.player += 1
+                    draw_score(self,self.player,self.dealer)
+                else:
+                    self.dealer += 1
+                    draw_score(self,self.player,self.dealer)
+            self.saved_value = self.player
+            return self.player,self.dealer
+        else:
+            self.saved_value = self.player
+            self.switch = False
+            return self.player,self.dealer
     
     def player_high_score(self):
+        self.switch = True
         player_value, _ = self.which_is_it(self.player, 0)
-        return player_value
+        return self.saved_value
 
-    def get_player_score(self):
-        _, _ = self.which_is_it(self.player, 0) 
-        return self.player
--- seurapeli/src/services/write_to_file.py
+++ seurapeli/src/services/write_to_file.py
@@ -2,11 +2,11 @@ def write_to_file(data):
     with open('src/high_score_file.txt', "r") as file:
         old_content = file.read()
 
-    # Compare the sizes of the old and new content
-    if int(data) > len(old_content):
-        # Open the file in write mode and overwrite the old content with the new content
+    old_content = int(old_content) if old_content else 0
+
+    if int(data) > old_content:
         with open('src/high_score_file.txt', "w") as file:
-            file.write(str(data))  # Convert data to a string before writing
+            file.write(str(data))
         return data
     else:
         return old_content
